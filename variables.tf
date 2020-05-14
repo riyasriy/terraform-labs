7a9edb8c7ad14777913aa60c9e2dfaf72070c908
Riyas Shahul Hameed <Riyasshahul.Hameed@datacom.com.au>
2020-05-14 15:53:29 +0800

    commit

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "loc" {
+    description = "Default Azure Region"
+    default     = "West Europe"
+
+}
+
+
+variable "tags" {
+    default = {
+        source = "citadel"
+        env = "training"
+
+    }
+
+}
+
